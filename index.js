0afe8cc6b4521a2ad3f5356ff418280d80bf897c
Zmulvihill <zmulvihi@yahoo.com>
2023-07-09 17:16:28 -0700

    lines 1-18 to index.js and saved

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function App() {
-    return <div>Hello World</div>
+    const [displayTime, setDisplayTime] = React.useState(25 * 60);
+
+    const formatTime = (time) => {
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        return (
+            (minutes < 10 ? "0" + minutes : minutes) + ":" +
+            (seconds < 10 ? "0" + seconds : seconds)
+        );
+    };
+
+    return ( 
+    <div>
+        <h1>{formatTime(displayTime)}</h1>
+    </div>
+    );
 }
 
 
