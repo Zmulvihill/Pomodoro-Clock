d4f5a1e000f55e50802e51ad2e43ddcd8cda6ce7
Zmulvihill <zmulvihi@yahoo.com>
2023-07-17 19:04:05 -0700

    removed import ./App from index.js, changed function in app.js and saved

--- index.js
+++ index.js
@@ -1,61 +1,15 @@
-function App() {
-    const [displayTime, setDisplayTime] = React.useState(25 * 60);
-    const [breakTime, setBreakTime] = React.useState(5 * 60);
-    const [sessionTime, setSessionTime] = React.useState(25 * 60);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
-    const formatTime = (time) => {
-        let minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        return (
-            (minutes < 10 ? "0" + minutes : minutes) + ":" +
-            (seconds < 10 ? "0" + seconds : seconds)
-        );
-    };
-
-    const changeTime = (amount, type) => {
-        if (type == "break") {
-            setBreakTime((prev) => prev + amount);
-        }
-    };
-
-    return ( 
-    <div>
-        <Length
-        title={"break length"}
-        changeTime={changeTime}
-        type={"break"}
-        time={breakTime}
-        formatTime={formatTime}
-        />
-        <Length
-        title={"session length"}
-        changeTime={changeTime}
-        type={"session"}
-        time={sessionTime}
-        formatTime={formatTime}
-        />
-        <h1>{formatTime(displayTime)}</h1>
-    </div>
-    );
-}
-
-function Length({ title, changeTime, type, time, formatTime}) {
-    return (
-        <div>
-            <h3>{title}</h3>
-            <div className="time-sets">
-                <button className="btn-small deep-purple lighten-2"
-                        onClick={() => changeTime(-60, type)}>
-                    <i className="material-icons">arrow_downward</i>
-                </button>
-                <h3>{formatTime(time)}</h3>
-                <button className="btn-small deep-purple lighten-2"
-                        onClick={() => changeTime(60, type)}>
-                    <i className="material-icons">arrow_upward</i>
-                </button>
-            </div>
-        </div>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
